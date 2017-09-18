32f8da5705ff642dd2cb91166e6d624b7e6c521c
Eric Marcon <eric.marcon@ecofog.gf>
2017-09-17 22:27:55 -0300

    Version initiale.
    Modèles d'article et de présentation.
    Requêtes d'accès aux données Guyafor.

---
+++ R/EcofoG.R
@@ -0,0 +1,34 @@
+#' EcoFoG
+#'
+#' Package rassemblant les outils communs à l'UMR EcoFoG dans R
+#'
+#' @name EcoFoG
+#' @docType package
+NULL
+
+#' Article
+#'
+#' Formatage d'un article pour l'auto-archivage
+#'
+#' La fonction est appelée par le modèle Markdown article
+#' @export
+article <- function (..., md_extensions = c("-autolink_bare_uris")) {
+  # Largement inspiré du package rticles
+  find_file <- function (template, file) {
+    template <- system.file("rmarkdown", "templates", template, file, package = "EcoFoG")
+    if (template == "") {
+      stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+    }
+    template
+  }
+  find_resource <- function (template, file) {
+    find_file(template, file.path("resources", file))
+  }
+  inherit_pdf_document <- function (...) {
+    fmt <- rmarkdown::pdf_document(...)
+    fmt$inherits <- "pdf_document"
+    fmt
+  }
+
+  inherit_pdf_document(..., template = find_resource("article", "template.tex"), md_extensions = md_extensions, citation_package = "natbib")
+}
---
+++ R/Guyafor.R
@@ -0,0 +1,60 @@
+#' Donnees Guyafor
+#'
+#' Importation de l'ensemble des données de la base Guyafor dans un dataframe
+#'
+#' La fonction exécute une requête sur le serveur sql.ecofog.gf pour lire les données
+#' @export
+
+Guyafor2df <- function () {
+  # Connection odbc à Guyafor sur serveur SQL
+  Connex <- rodbc::odbcConnect(dsn="Guyafor")
+
+  # Sélection des données
+
+  req1 <- "SELECT dbo.TtGuyaforShiny.NomForet, dbo.TtGuyaforShiny.n_parcelle, dbo.TtGuyaforShiny.n_carre, dbo.TtGuyaforShiny.n_arbre, dbo.TtGuyaforShiny.Surface,
+                  dbo.TtGuyaforShiny.i_arbre, dbo.TtGuyaforShiny.X, dbo.TtGuyaforShiny.Y, dbo.TtGuyaforShiny.Xutm, dbo.TtGuyaforShiny.Yutm, dbo.TtGuyaforShiny.UTMZone,
+                  dbo.TtGuyaforShiny.Lat, dbo.TtGuyaforShiny.Lon, dbo.TtGuyaforShiny.n_essence, dbo.TtGuyaforShiny.idPilote, dbo.TtGuyaforShiny.nomPilote,
+                  dbo.TtGuyaforShiny.Densite, dbo.TtGuyaforShiny.circonf, dbo.TtGuyaforShiny.code_vivant, dbo.TtGuyaforShiny.code_mesure, dbo.TtGuyaforShiny.campagne,
+                  dbo.TtGuyaforShiny.DateMesure, dbo.taMesure_Corr.circ_corr, dbo.taMesure_Corr.code_corr, dbo.TtGuyaforShiny.Famille, dbo.TtGuyaforShiny.Genre,
+                  dbo.TtGuyaforShiny.Espece, dbo.TtGuyaforShiny.Commerciale, dbo.TtGuyaforShiny.SourceBota, dbo.TtGuyaforShiny.indSurete
+           FROM   dbo.TtGuyaforShiny INNER JOIN
+                  dbo.taMesure_Corr ON dbo.TtGuyaforShiny.idMesure = dbo.taMesure_Corr.idMesure"
+
+  rodbc::sqlQuery(Connex,req1) -> dfGuyafor
+
+  # Clôture de la connection odbc
+  rodbc::odbcClose(Connex)
+}
+
+
+
+
+#' Donnees Paracou
+#'
+#' Importation de l'ensemble des données de Paracou de la base Guyafor dans un dataframe
+#'
+#' La fonction exécute une requête sur le serveur sql.ecofog.gf pour lire les données
+#' @export
+
+Paracou2df <- function () {
+  # Connection odbc à Guyafor sur serveur SQL
+  Connex <- rodbc::odbcConnect(dsn="Guyafor")
+
+  # Sélection des données
+
+  req1 <- "SELECT dbo.TtGuyaforShiny.NomForet, dbo.TtGuyaforShiny.n_parcelle, dbo.TtGuyaforShiny.n_carre, dbo.TtGuyaforShiny.n_arbre, dbo.TtGuyaforShiny.Surface,
+                  dbo.TtGuyaforShiny.i_arbre, dbo.TtGuyaforShiny.X, dbo.TtGuyaforShiny.Y, dbo.TtGuyaforShiny.Xutm, dbo.TtGuyaforShiny.Yutm, dbo.TtGuyaforShiny.UTMZone,
+                  dbo.TtGuyaforShiny.Lat, dbo.TtGuyaforShiny.Lon, dbo.TtGuyaforShiny.n_essence, dbo.TtGuyaforShiny.idPilote, dbo.TtGuyaforShiny.nomPilote,
+                  dbo.TtGuyaforShiny.Densite, dbo.TtGuyaforShiny.circonf, dbo.TtGuyaforShiny.code_vivant, dbo.TtGuyaforShiny.code_mesure, dbo.TtGuyaforShiny.campagne,
+                  dbo.TtGuyaforShiny.DateMesure, dbo.taMesure_Corr.circ_corr, dbo.taMesure_Corr.code_corr, dbo.TtGuyaforShiny.Famille, dbo.TtGuyaforShiny.Genre,
+                  dbo.TtGuyaforShiny.Espece, dbo.TtGuyaforShiny.Commerciale, dbo.TtGuyaforShiny.SourceBota, dbo.TtGuyaforShiny.indSurete
+           FROM   dbo.TtGuyaforShiny INNER JOIN
+                  dbo.taMesure_Corr ON dbo.TtGuyaforShiny.idMesure = dbo.taMesure_Corr.idMesure
+           WHERE  (dbo.TtGuyaforShiny.NomForet = N'paracou')"
+
+  rodbc::sqlQuery(Connex,req1) -> dfParacou
+
+  # Clôture de la connection odbc
+  rodbc::odbcClose(Connex)
+}
+
