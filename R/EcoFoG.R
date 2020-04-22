5f4b8355bc0b1df8371a7b9b7677efbf97a3c2e1
Eric Marcon <Eric.Marcon@ecofog.gf>
2020-04-22 19:37:08 -0300

    Casse du nom de fichier

---
+++ R/EcoFoG.R
@@ -0,0 +1,96 @@
+#' EcoFoG
+#'
+#' Package rassemblant les outils communs à l'UMR EcoFoG dans R
+#'
+#' @name EcoFoG
+#' @docType package
+#' @import magrittr
+NULL
+
+
+# Fonctions internes
+#  Utilitaires pour article() et memo()
+#  Largement inspirées du package rticles
+#' @keywords internal
+find_file <- function (template, file) {
+  template <- system.file("rmarkdown", "templates", template, file, package = "EcoFoG")
+  if (template == "") {
+    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+  }
+  return(template)
+}
+#' @keywords internal
+find_resource <- function (template, file) {
+  return(find_file(template, file.path("resources", file)))
+}
+#' @keywords internal
+inherit_pdf_document <- function (...) {
+  fmt <- rmarkdown::pdf_document(...)
+  fmt$inherits <- "pdf_document"
+  return(fmt)
+}
+
+#' Article
+#'
+#' Formatage d'un article pour l'auto-archivage
+#'
+#' La fonction est appelée par le modèle Markdown article
+#'
+#' @param ... Arguments optionnels passés à \code{\link{pdf_document}}
+#' @param md_extensions Extensions markdodown, cf. \code{\link{pdf_document}}
+#'
+#' @export
+article <- function (..., md_extensions = c("-autolink_bare_uris")) {
+  inherit_pdf_document(..., template = find_resource("article", "template.tex"), md_extensions = md_extensions, citation_package = "natbib")
+}
+
+#' Memo
+#'
+#' Formatage d'un mémo
+#'
+#' La fonction est appelée par le modèle Markdown memo
+#'
+#' @inheritParams article
+#'
+#' @export
+memo <- function (..., md_extensions = c("-autolink_bare_uris")) {
+  inherit_pdf_document(..., template = find_resource("memo", "template.tex"), md_extensions = md_extensions, citation_package = "natbib")
+}
+
+
+
+#' TricoterTout
+#'
+#' Création des tous les documents à partir des modèles
+#'
+#' Utilisé pour vérifier le bon fonctionnement des modèles
+#'
+#' @param destination Dossier de destination des documents
+#'
+#' @export
+TricoterTout <- function (destination="docs") {
+  # Article
+  wd <- getwd()
+  tmpdir <- tempdir()
+  setwd(tmpdir)
+  rmarkdown::draft("article", template="article", package="EcoFoG", edit=FALSE)
+  setwd("article")
+  # Knit to HTML
+  rmarkdown::render(input="article.Rmd", output_format=bookdown::html_document2(), output_dir = "docs")
+  # Knit to pdf
+  rmarkdown::render(input="article.Rmd", output_format=bookdown::pdf_book(base_format = EcoFoG::article), output_dir = "docs")
+  # Copy to destination
+  docsDirs <- list.dirs(path="docs", full.names=TRUE, recursive=TRUE)
+  dir.create(paste(wd, "/", destination, sep = ""))
+  dir.create(paste(wd, "/", destination, "/article", sep = ""))
+  if (length(docsDirs) > 0) {
+    sapply(paste(wd, "/", destination, "/article/", docsDirs, sep=""), dir.create)
+    docsFiles <- list.files("docs", full.names=TRUE, recursive=TRUE)
+    file.copy(from=docsFiles, to=paste(wd, "/", destination, "/article/", docsFiles, sep = ""), overwrite=TRUE)
+  }
+  # Clean up
+  setwd(wd)
+  unlink(paste(tmpdir, "/article", sep = ""), recursive = TRUE)
+}
+
+
