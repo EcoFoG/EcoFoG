4cfa03ae86e6d049b8135c6ccd3ab3dfb98c9f72
Eric Marcon <Eric.Marcon@ecofog.gf>
2020-04-21 22:50:53 -0300

    Knit commands corrected

--- tests/testthat/testArticle.R
+++ tests/testthat/testArticle.R
@@ -5,12 +5,16 @@ testthat::test_that("Coverage is estimated", {
   testthat::skip_on_cran()
   # create an article project
   if (rmarkdown::pandoc_available()) {
+    # Create a project
     rmarkdown::draft("test_article", template="article", package="EcoFoG", edit=FALSE)
     setwd("test_article/")
-    test_result <- rmarkdown::render("test_article.Rmd", "html_document")
+    # Knit in HTML
+    test_result <- rmarkdown::render(input="test_article.Rmd", output_format=bookdown::html_document2())
     testthat::expect_true(endsWith(test_result, "test_article.html"))
-    test_result <- rmarkdown::render("test_article.Rmd", "pdf_document")
+    # Knit in pdf
+    test_result <- rmarkdown::render("test_article.Rmd", "pdf_book")
     testthat::expect_true(endsWith(test_result, "test_article.pdf"))
+    # Clean up
     setwd("..")
     unlink("test_article", recursive = TRUE)
   }
