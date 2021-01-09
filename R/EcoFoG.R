1098df3e9046558369e00b1df9418be3021c91f0
Eric Marcon <e.marcon@free.fr>
2021-01-09 20:22:40 +0100

    destination = argument

--- R/EcoFoG.R
+++ R/EcoFoG.R
@@ -13,9 +13,9 @@ NULL
 #  Largement inspirées du package rticles
 #' @keywords internal
 find_file <- function (template, file) {
-  template <- system.file("rmarkdown", "templates", template, file, package = "EcoFoG")
+  template <- system.file("rmarkdown", "templates", template, file, package="EcoFoG")
   if (template == "") {
-    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+    stop("Couldn't find template file ", template, "/", file, call.=FALSE)
   }
   return(template)
 }
@@ -40,8 +40,8 @@ inherit_pdf_document <- function (...) {
 #' @param md_extensions Extensions markdodown, cf. \code{\link{pdf_document}}
 #'
 #' @export
-article <- function (..., md_extensions = c("-autolink_bare_uris")) {
-  inherit_pdf_document(..., template = find_resource("article", "template.tex"), md_extensions = md_extensions, citation_package = "natbib")
+article <- function (..., md_extensions=c("-autolink_bare_uris")) {
+  inherit_pdf_document(..., template=find_resource("article", "template.tex"), md_extensions=md_extensions, citation_package="natbib")
 }
 
 #' Memo
@@ -53,8 +53,8 @@ article <- function (..., md_extensions = c("-autolink_bare_uris")) {
 #' @inheritParams article
 #'
 #' @export
-memo <- function (..., md_extensions = c("-autolink_bare_uris")) {
-  inherit_pdf_document(..., template = find_resource("memo", "template.tex"), md_extensions = md_extensions, citation_package = "natbib")
+memo <- function (..., md_extensions=c("-autolink_bare_uris")) {
+  inherit_pdf_document(..., template=find_resource("memo", "template.tex"), md_extensions=md_extensions, citation_package="natbib")
 }
 
 
@@ -89,32 +89,32 @@ TricoterArticle <- function (destination="docs") {
   tmpdir <- tempdir()
   # Article
   setwd(tmpdir)
-  unlink("article", recursive = TRUE)
+  unlink("article", recursive=TRUE)
   rmarkdown::draft("article", template="article", package="EcoFoG", edit=FALSE)
   setwd("article")
   # Knit to HTML
-  options(knitr.table.format = 'html')
+  options(knitr.table.format='html')
   rmarkdown::render(input="article.Rmd",
                     output_format=bookdown::html_document2(theme="sandstone", toc=TRUE, toc_float=TRUE),
-                    output_dir = "docs")
+                    output_dir=destination)
   # Knit to pdf
-  options(knitr.table.format = 'latex')
+  options(knitr.table.format='latex')
   rmarkdown::render(input="article.Rmd",
-                    output_format=bookdown::pdf_book(base_format = EcoFoG::article, latex_engine=xelatex),
-                    output_dir = "docs")
+                    output_format=bookdown::pdf_book(base_format=EcoFoG::article, latex_engine="xelatex"),
+                    output_dir=destination)
   # Copy to destination
-  docsDirs <- list.dirs(path="docs", full.names=TRUE, recursive=TRUE)
-  dir.create(paste(OriginalWD, "/", destination, sep = ""), showWarnings=FALSE)
-  dir.create(paste(OriginalWD, "/", destination, "/article", sep = ""), showWarnings=FALSE)
+  docsDirs <- list.dirs(path=destination, full.names=TRUE, recursive=TRUE)
+  dir.create(paste(OriginalWD, "/", destination, sep=""), showWarnings=FALSE)
+  dir.create(paste(OriginalWD, "/", destination, "/article", sep=""), showWarnings=FALSE)
   if (length(docsDirs) > 0) {
     sapply(paste(OriginalWD, "/", destination, "/article/", docsDirs, sep=""), dir.create, showWarnings=FALSE)
-    docsFiles <- list.files("docs", full.names=TRUE, recursive=TRUE)
-    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/article/", docsFiles, sep = ""), overwrite=TRUE)
+    docsFiles <- list.files(destination, full.names=TRUE, recursive=TRUE)
+    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/article/", docsFiles, sep=""), overwrite=TRUE)
   }
   # Clean up
   setwd(OriginalWD)
-  unlink(paste(tmpdir, "/article", sep = ""), recursive = TRUE)
-  options(knitr.table.format = knitr_table_format)
+  unlink(paste(tmpdir, "/article", sep=""), recursive=TRUE)
+  options(knitr.table.format=knitr_table_format)
 }
 
 #' @rdname Tricoter
@@ -126,34 +126,34 @@ TricoterPresentation <- function (destination="docs") {
   tmpdir <- tempdir()
   # Beamer
   setwd(tmpdir)
-  unlink("beamer", recursive = TRUE)
+  unlink("beamer", recursive=TRUE)
   rmarkdown::draft("beamer", template="beamer", package="EcoFoG", edit=FALSE)
   setwd("beamer")
   # Knit to HTML
-  options(knitr.table.format = 'html')
+  options(knitr.table.format='html')
   rmarkdown::render(input="beamer.Rmd",
                     output_format=rmarkdown::ioslides_presentation(
-                      logo="images/EcoFoG2020.png", widescreen=TRUE), output_dir = "docs")
+                      logo="images/EcoFoG2020.png", widescreen=TRUE), output_dir=destination)
   # Knit to pdf
-  options(knitr.table.format = 'latex')
+  options(knitr.table.format='latex')
   rmarkdown::render(input="beamer.Rmd",
                     output_format=rmarkdown::beamer_presentation(
                       includes=list(in_header="EcoFoGBeamer.tex"),
                       df_print="kable", fig_caption=FALSE, slide_level=2),
-                    output_dir = "docs")
+                    output_dir=destination)
   # Copy to destination
-  docsDirs <- list.dirs(path="docs", full.names=TRUE, recursive=TRUE)
-  dir.create(paste(OriginalWD, "/", destination, sep = ""), showWarnings=FALSE)
-  dir.create(paste(OriginalWD, "/", destination, "/beamer", sep = ""), showWarnings=FALSE)
+  docsDirs <- list.dirs(path=destination, full.names=TRUE, recursive=TRUE)
+  dir.create(paste(OriginalWD, "/", destination, sep=""), showWarnings=FALSE)
+  dir.create(paste(OriginalWD, "/", destination, "/beamer", sep=""), showWarnings=FALSE)
   if (length(docsDirs) > 0) {
     sapply(paste(OriginalWD, "/", destination, "/beamer/", docsDirs, sep=""), dir.create, showWarnings=FALSE)
-    docsFiles <- list.files("docs", full.names=TRUE, recursive=TRUE)
-    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/beamer/", docsFiles, sep = ""), overwrite=TRUE)
+    docsFiles <- list.files(destination, full.names=TRUE, recursive=TRUE)
+    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/beamer/", docsFiles, sep=""), overwrite=TRUE)
   }
   # Clean up
   setwd(OriginalWD)
-  unlink(paste(tmpdir, "/beamer", sep = ""), recursive = TRUE)
-  options(knitr.table.format = knitr_table_format)
+  unlink(paste(tmpdir, "/beamer", sep=""), recursive=TRUE)
+  options(knitr.table.format=knitr_table_format)
 }
 
 #' @rdname Tricoter
@@ -165,29 +165,29 @@ TricoterOuvrage <- function (destination="docs") {
   tmpdir <- tempdir()
   # Book
   setwd(tmpdir)
-  unlink("book", recursive = TRUE)
+  unlink("book", recursive=TRUE)
   rmarkdown::draft("book", template="book", package="EcoFoG", edit=FALSE)
   setwd("book")
   unlink("book.Rmd")
   # Knit to HTML
-  options(knitr.table.format = 'html')
+  options(knitr.table.format='html')
   bookdown::render_book("index.Rmd", "bookdown::gitbook")
   # Knit to pdf
-  options(knitr.table.format = 'latex')
+  options(knitr.table.format='latex')
   bookdown::render_book("index.Rmd", "bookdown::pdf_book")
   # Copy to destination
-  docsDirs <- list.dirs(path="docs", full.names=TRUE, recursive=TRUE)
-  dir.create(paste(OriginalWD, "/", destination, sep = ""), showWarnings=FALSE)
-  dir.create(paste(OriginalWD, "/", destination, "/book", sep = ""), showWarnings=FALSE)
+  docsDirs <- list.dirs(path=destination, full.names=TRUE, recursive=TRUE)
+  dir.create(paste(OriginalWD, "/", destination, sep=""), showWarnings=FALSE)
+  dir.create(paste(OriginalWD, "/", destination, "/book", sep=""), showWarnings=FALSE)
   if (length(docsDirs) > 0) {
     sapply(paste(OriginalWD, "/", destination, "/book/", docsDirs, sep=""), dir.create, showWarnings=FALSE)
-    docsFiles <- list.files("docs", full.names=TRUE, recursive=TRUE)
-    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/book/", docsFiles, sep = ""), overwrite=TRUE)
+    docsFiles <- list.files(destination, full.names=TRUE, recursive=TRUE)
+    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/book/", docsFiles, sep=""), overwrite=TRUE)
   }
   # Clean up
   setwd(OriginalWD)
-  unlink(paste(tmpdir, "/book", sep = ""), recursive = TRUE)
-  options(knitr.table.format = knitr_table_format)
+  unlink(paste(tmpdir, "/book", sep=""), recursive=TRUE)
+  options(knitr.table.format=knitr_table_format)
 }
 
 #' @rdname Tricoter
@@ -199,26 +199,26 @@ TricoterMemo <- function (destination="docs") {
   tmpdir <- tempdir()
   # Memo
   setwd(tmpdir)
-  unlink("memo", recursive = TRUE)
+  unlink("memo", recursive=TRUE)
   rmarkdown::draft("memo", template="memo", package="EcoFoG", edit=FALSE)
   setwd("memo")
   # Knit to HTML
-  options(knitr.table.format = 'html')
-  rmarkdown::render(input="memo.Rmd", output_format=bookdown::html_document2(), output_dir = "docs")
+  options(knitr.table.format='html')
+  rmarkdown::render(input="memo.Rmd", output_format=bookdown::html_document2(), output_dir=destination)
   # Knit to pdf
-  options(knitr.table.format = 'latex')
-  rmarkdown::render(input="memo.Rmd", output_format=bookdown::pdf_book(base_format = EcoFoG::memo), output_dir = "docs")
+  options(knitr.table.format='latex')
+  rmarkdown::render(input="memo.Rmd", output_format=bookdown::pdf_book(base_format=EcoFoG::memo), output_dir=destination)
   # Copy to destination
-  docsDirs <- list.dirs(path="docs", full.names=TRUE, recursive=TRUE)
-  dir.create(paste(OriginalWD, "/", destination, sep = ""), showWarnings=FALSE)
-  dir.create(paste(OriginalWD, "/", destination, "/memo", sep = ""), showWarnings=FALSE)
+  docsDirs <- list.dirs(path=destination, full.names=TRUE, recursive=TRUE)
+  dir.create(paste(OriginalWD, "/", destination, sep=""), showWarnings=FALSE)
+  dir.create(paste(OriginalWD, "/", destination, "/memo", sep=""), showWarnings=FALSE)
   if (length(docsDirs) > 0) {
     sapply(paste(OriginalWD, "/", destination, "/memo/", docsDirs, sep=""), dir.create, showWarnings=FALSE)
-    docsFiles <- list.files("docs", full.names=TRUE, recursive=TRUE)
-    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/memo/", docsFiles, sep = ""), overwrite=TRUE)
+    docsFiles <- list.files(destination, full.names=TRUE, recursive=TRUE)
+    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/memo/", docsFiles, sep=""), overwrite=TRUE)
   }
   # Clean up
   setwd(OriginalWD)
-  unlink(paste(tmpdir, "/memo", sep = ""), recursive = TRUE)
-  options(knitr.table.format = knitr_table_format)
+  unlink(paste(tmpdir, "/memo", sep=""), recursive=TRUE)
+  options(knitr.table.format=knitr_table_format)
 }
